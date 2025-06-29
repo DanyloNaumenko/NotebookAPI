e27ed57174247872c8d4ac66dbb4332b36e02370
Danylo Namuenko <d.naumenko1111@gmail.com>
2025-06-29 18:26:42 +0200

    Changed DateOfWatching into PeriodOfWatching in AnimeNote.cs

--- AnimeNotesAPI/AnimeNote.cs
+++ AnimeNotesAPI/AnimeNote.cs
@@ -7,7 +7,7 @@ namespace AnimeNotesAPI;
 public record class AnimeNote : Note
 {
     public int Rating { get; init; }
-    public DateTime DateOfWatching { get; init; }
+    public TimeSpan DateOfWatching { get; init; }
     public List<Genres> Genres { get; init; } = new();
     public byte[] Image { get; init; } = Array.Empty<byte>();
     public WatchingStatus Status { get; init; }
--- AnimeNotesAPI/Note.cs
+++ AnimeNotesAPI/Note.cs
@@ -3,7 +3,6 @@ namespace AnimeNotesAPI;
 public abstract record class Note
 {
     protected const string NO_DATA = "No data";
-    
     protected Note() { }
     public virtual string Title { get; init; } = NO_DATA;
     public virtual string Description { get; init; } = NO_DATA;
