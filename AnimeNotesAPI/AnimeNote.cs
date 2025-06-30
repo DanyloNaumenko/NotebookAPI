728cb311035671a3d6dc057c57a3edac9ac5d4e9
Danylo Namuenko <d.naumenko1111@gmail.com>
2025-06-30 23:26:52 +0200

    Added validation in NoteManager.cs

--- AnimeNotesAPI/AnimeNote.cs
+++ AnimeNotesAPI/AnimeNote.cs
@@ -4,10 +4,10 @@ using AnimeNotesAPI.enums;
 
 namespace AnimeNotesAPI;
 
-public record class AnimeNote : Note
+public record  AnimeNote : Note
 {
     public int Rating { get; init; }
-    public TimeSpan DateOfWatching { get; init; }
+    public TimeSpan PeriodOfWatching { get; init; }
     public List<Genres> Genres { get; init; } = new();
     public byte[] Image { get; init; } = Array.Empty<byte>();
     public WatchingStatus Status { get; init; }
--- AnimeNotesAPI/NoteManager.cs
+++ AnimeNotesAPI/NoteManager.cs
@@ -6,13 +6,22 @@ public abstract class NoteManager
 
     protected virtual bool AddNewNote(Note note)
     {
+        if (!Validation(note))
+        {
+            throw new ArgumentException("Input data is invalid! The adding was failed.");
+        }
         return _cachedNotes.TryAdd(note.Id, note);
         //TODO add logic with database
     }
 
     protected virtual bool UpdateNoteById(Guid id, Note newNote)
     {
+        if (!Validation(newNote))
+        {
+            throw new ArgumentException("Input data is invalid! Can not apply new note.");
+        }
         if (!_cachedNotes.ContainsKey(id)) throw new KeyNotFoundException();
+        
         _cachedNotes[id] = newNote;
         //TODO add logic with database
         return true;
@@ -28,6 +37,18 @@ public abstract class NoteManager
     protected virtual bool DeleteNoteById(Guid id)
     {
         //TODO add logic with database
+        if (!_cachedNotes.ContainsKey(id)) throw new KeyNotFoundException();
         return _cachedNotes.Remove(id);
+    }
+
+    protected virtual bool Validation(Note note)
+    {
+        bool isValid = true;
+
+        if (note.Id == Guid.Empty)
+        {
+            return false;
+        }
+        return isValid;
     }
 }
