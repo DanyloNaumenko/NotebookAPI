9b0775b34b2236640e30cbcbcbe5d347e79fdb4d
Науменко Станислав Юрьевич <d.naumenko1111@gmail.com>
2025-07-13 15:16:53 +0300

    Added GetAll() option in NoteProvider

--- Domain/Interfaces/INoteProvider.cs
+++ Domain/Interfaces/INoteProvider.cs
@@ -6,6 +6,7 @@ public interface INoteProvider
 {
     Note Create();
     Note GetById(Guid id);
+    ICollection<Note> GetAll();
     bool Update(Guid id, Note updatedNote);
     public bool Delete(Guid id);
 }
